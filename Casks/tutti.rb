7e4a1a7a444e88ac3bf35f1cfb6e3926dd64321b
answer24 <barry.wu.china@gmail.com>
2026-08-11 16:30:19 +0800

    bump tutti to 1.9.1

--- Casks/tutti.rb
+++ Casks/tutti.rb
@@ -1,6 +1,6 @@
 cask "tutti" do
-  version "1.9.0"
-  sha256 "1112b31b842ac4d047133b3439625af564123d8d32f2d12442e88b7201055d82"
+  version "1.9.1"
+  sha256 "4b461cf30956b0de5f7c5be3e94cd82cd2bbd735d616e50884c8ba5d021724e1"
 
   url "https://github.com/BarryBarrywu/tutti/releases/download/v#{version}/Tutti-#{version}.zip"
   name "Tutti"
